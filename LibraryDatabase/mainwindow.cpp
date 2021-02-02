a221038844219f38265b914f6b16cd5153e97aa8
Hubert Oszywa <hubertoszywa@gmail.com>
2021-02-02 21:05:32 +0100

    Kompiluje się :)

--- LibraryDatabase/mainwindow.cpp
+++ LibraryDatabase/mainwindow.cpp
@@ -3,6 +3,11 @@
 #include "QMessageBox"
 #include "userform.h"
 #include "bookform.h"
+#include <QString>
+#include <QList>
+#include <QStringList>
+#include <QDir>
+#include <QDebug>
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
@@ -28,7 +33,7 @@ void MainWindow::on_AddBookBtn_clicked()
 {
 
     BookForm bookForm(this);
-    QObject::connect(&bookForm,&BookForm::bookAdded,this,&MainWindow::onBookAdded);
+    QObject::connect(&bookForm,&BookForm::bookAdded,this,&MainWindow::onBookChanged);
     bookForm.exec();
 
     //Przykładowa obsługa listy
@@ -55,10 +60,37 @@ void MainWindow::on_EditBookBtn_clicked()
     bookForm.exec();
 }
 
-void MainWindow::onBookAdded(Book book)
+void MainWindow::onBookChanged(Book book)
 {
     this->_books.Add(book);
+    if(book.Id == 0)
+    {
+        this->_books.Add(book);
+        saveDataToFile(book);
+    }
+    else
+    {
+        Book *tmp = this->_books.FirstOrDefault([&](Book b){return b.Id == book.Id;});
+        tmp->Title = book.Title;
+        tmp->Author = book.Author;
+        tmp->PublishCountry = book.PublishCountry;
+        tmp->PublishDate = book.PublishDate;
+        tmp->IsbnNumber = book.IsbnNumber;
+    }
 }
+
+void MainWindow::saveDataToFile(Book book)
+{
+
+    QString filename = "Data.csv";
+    QFile file(filename);
+    if (file.open(QIODevice::WriteOnly | QIODevice::Append))
+    {
+       QTextStream stream(&file);
+       stream << QString::number(book.Id) <<","<< book.Title.c_str()<<"," << book.Author.c_str()<<"," << book.PublishDate.c_str()<<"," << book.PublishCountry.c_str()<<"," << book.IsbnNumber.c_str() << Qt::endl;
+    }
+}
+
 
 void MainWindow::on_DeleteBookBtn_clicked()
 {
--- LibraryDatabase/mainwindow.h
+++ LibraryDatabase/mainwindow.h
@@ -30,7 +30,11 @@ private slots:
 
     void on_EditBookBtn_clicked();
 
-    void onBookAdded(Book book);
+    void saveDataToFile(Book book);
+
+    void onBookChanged(Book book);
+
+    //void onBookAdded(Book book);
 private:
     Ui::MainWindow *ui;
     List<Book> _books;
